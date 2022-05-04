eceb179aa2fc07183eef9ce2720116c78931b90d
cockatiella <sofiadinatolo@hotmail.com>
2022-05-04 21:19:05 +0200

    solution

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 //Write a JavaScript function that accepts a string as a parameter and converts the first letter of each word of the string in upper case.
 // 1
 function uppercase(str) {
-  //write your code here 
+let myArr = str.split(' ');
+let newArray = [];
+
+for (let i = 0; i < myArr.length; i++){
+  newArray.push(myArr[i].charAt(0).toUpperCase()+myArr[i].slice(1));
+}
+return newArray.join(' ');
 }
 
 console.log(uppercase("the quick brown fox"));
@@ -9,16 +15,25 @@ console.log(uppercase("the quick brown fox"));
 // 2
 // Write a JavaScript function that accepts a string as a parameter and find the longest word within the string.
 function find_longest_word(str) {
-  //write your code here 
+ let strSplit = str.split(' ');
+ let longestWord = 0;
+ for (let i = 0; i < strSplit.length; i++){
+   if(strSplit[i].length > longestWord){
+     longestWord = strSplit[i].length;
+   }
+ }
+ return longestWord;
 }
 console.log(find_longest_word("Web Development Tutorial"));
 
 // 3
 // Write a JavaScript function that accept a list of country names as input and returns the longest country name as output.
-function Longest_Country_Name(country_name) {
-  //write your code here 
-}
+function country(array){
+  array.sort(function(a,b){return b.length-a.length});
+      return array[0];
+  }
+
 
 console.log(
-  Longest_Country_Name(["Australia", "Germany", "United States of America"])
+  country(["Australia", "Germany", "United States of America"])
 );
