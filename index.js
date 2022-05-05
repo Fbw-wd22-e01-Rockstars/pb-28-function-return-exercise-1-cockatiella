923cdbdb519980815a08b4392719a61fa5fa3d2a
cockatiella <sofiadinatolo@hotmail.com>
2022-05-05 15:19:39 +0200

    changes

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ console.log(uppercase("the quick brown fox"));
 // Write a JavaScript function that accepts a string as a parameter and find the longest word within the string.
 function find_longest_word(str) {
  let strSplit = str.split(' ');
- let longestWord = 0;
+ let longestWord = strSplit[0];
  for (let i = 0; i < strSplit.length; i++){
-   if(strSplit[i].length > longestWord){
-     longestWord = strSplit[i].length;
+   if(strSplit[i].length > longestWord.length){
+     longestWord = strSplit[i];
    }
  }
  return longestWord;
@@ -29,8 +29,13 @@ console.log(find_longest_word("Web Development Tutorial"));
 // 3
 // Write a JavaScript function that accept a list of country names as input and returns the longest country name as output.
 function country(array){
-  array.sort(function(a,b){return b.length-a.length});
-      return array[0];
+  let longestWord = array[0];
+  for (let i = 0; i < array.length; i++){
+    if(array[i].length > longestWord.length){
+      longestWord = array[i];
+    }
+  }
+  return longestWord;
   }
 
 
